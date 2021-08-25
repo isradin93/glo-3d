56506f0595d946c3024ae1fb49dd6bf4c57a35e1
Isradin <isradin.nurbek@gmail.com>
2021-08-25 19:47:24 +0300

    Create task 28.1

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+
 
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
 
-    const getData = () => {
-        return new Promise((resolve, reject) => {
-            const request = new XMLHttpRequest();
-            request.open('GET', './cars.json');
-            request.setRequestHeader('Content-type', 'application/json');
-            request.addEventListener('readystatechange', () => {
-                if (request.readyState !== 4) {
-                    return;
-                }
+    const getData = () => new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', './cars.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) {
+                return;
+            }
 
-                if (request.status === 200) {
-                    const data = JSON.parse(request.responseText);
-                    resolve(data);
-                } else {
-                    reject(request.status);
-                    output.innerHTML = 'Произошла ошибка';
-                }
-            });
-            request.send();
+            if (request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else {
+                reject(request.status);
+                output.innerHTML = 'Произошла ошибка';
+            }
         });
-    };
+        request.send();
+    });
 
     const outputData = data => {
         data.cars.forEach(item => {
